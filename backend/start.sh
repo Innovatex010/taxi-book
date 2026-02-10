23183521be9455c7f72943bf700c40a00e02cf0e
Innovatex010 <innovatex010@gmail.com>
2026-02-10 11:14:06 +0000

    doene

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
-sudo apt update
-sudo apt install ca-certificates curl gnupg lsb-release -y
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt update
+apt install ca-certificates curl gnupg lsb-release -y
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 docker run -d --name taxi-mongo -p 27017:27017 mongo:6.0
 
 echo "Starting uvicorn on port ${PORT} (reload enabled)..."
